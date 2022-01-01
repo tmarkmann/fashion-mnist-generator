1d558056a427632bc87b06858ba598b49a2e6a8e
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-01 15:46:50 +0100

    Added script to show 25 images.

--- scripts/show_dataset.py
+++ scripts/show_dataset.py
@@ -1,1 +1,18 @@
-# TODO
+import matplotlib.pyplot as plt
+
+from dataloader.fashion_mnist import FashionMNISTDataset
+import tensorflow as tf
+
+ds = FashionMNISTDataset()
+
+def show_examples():
+    data = ds.ds_train.take(25)
+    plt.figure(figsize=(10, 10))
+    for index, example in enumerate(data):
+        image = example[0].numpy()
+        image = tf.image = image.reshape((28,28))
+        plt.subplot(5, 5, index+1)
+        plt.imshow(image, cmap=plt.cm.binary)
+    plt.show()
+
+show_examples()
