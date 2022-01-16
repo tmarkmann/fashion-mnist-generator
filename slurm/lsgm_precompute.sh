8da3aaa2b000e37b2b9d67805c72761dae8719df
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2022-01-16 02:48:42 +0100

    Update lsgm precompute slurm script

--- slurm/lsgm_precompute.sh
+++ slurm/lsgm_precompute.sh
@@ -5,4 +5,5 @@ conda activate lsgm
 export TFDS_DATA_DIR=~/tensorflow_datasets
 export fid_dir=~/lsgm_fashion/fid
 
+cd ../LSGM
 python3 -m scripts.precompute_fid_statistics --data $TFDS_DATA_DIR/fashion_mnist --dataset fashion-mnist --fid_dir $fid_dir
