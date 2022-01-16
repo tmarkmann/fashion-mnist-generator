3a08bb76f959fe1e9e203698aa70ebb4513fb849
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2022-01-16 17:50:57 +0100

    Add lsgm eval scripts

---
+++ slurm/lsgm_eval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /media/compute/homes/tmarkmann/miniconda3/etc/profile.d/conda.sh
+conda activate lsgm
+
+export DATA_DIR=~/lsgm_fashion/dataset
+export FID_STATS_DIR=~/lsgm_fashion/fid
+export CHECKPOINT_DIR=~/lsgm_fashion/checkpoint
+export EXPR_ID=1
+
+cd ../LSGM
+python3 evaluate_vae.py --data $DATA_DIR/mnist --root $CHECKPOINT_DIR --save $EXPR_ID/eval_vae --eval_mode evaluate \
+        --checkpoint $CHECKPOINT_DIR/EXPR_ID/vae/checkpoint.pt --num_process_per_node 2 --fid_dir $FID_STATS_DIR \
+        --fid_eval --nll_eval
