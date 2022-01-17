6427d25d99d4133bfac884ed7100d50db4bcf089
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2022-01-17 03:26:47 +0100

    Fix lsgm train script again

--- slurm/lsgm_train.sh
+++ slurm/lsgm_train.sh
@@ -13,5 +13,5 @@ python3 train_vada.py --data $DATA_DIR/fashion-mnist --root $CHECKPOINT_DIR --sa
         --weight_decay_norm_dae 0. --num_channels_dae 256 --train_vae  --num_cell_per_scale_dae 8 \
         --learning_rate_dae 3e-4 --learning_rate_min_dae 3e-4 --train_ode_solver_tol 1e-5 --cont_kl_anneal  \
         --sde_type vpsde --iw_sample_p ll_iw --num_process_per_node 2 --use_se \
-        --vae_checkpoint $CHECKPOINT_DIR/EXPR_ID/vae/checkpoint.pt  --dae_arch ncsnpp --embedding_scale 1000 \
+        --vae_checkpoint $CHECKPOINT_DIR/$EXPR_ID/vae/checkpoint.pt  --dae_arch ncsnpp --embedding_scale 1000 \
         --mixing_logit_init -6 --warmup_epochs 20 --drop_inactive_var --skip_final_eval --fid_dir $FID_STATS_DIR
