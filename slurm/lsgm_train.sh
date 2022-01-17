3bb473f6f83a465aa51e57c8e552e7e2e2863787
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2022-01-17 03:22:53 +0100

    Add lsgm train scripts

--- slurm/lsgm_train.sh
+++ slurm/lsgm_train.sh
@@ -8,9 +8,10 @@ export CHECKPOINT_DIR=~/lsgm_fashion/checkpoint
 export EXPR_ID=1
 
 cd ../LSGM
-python3 train_vae.py --data $DATA_DIR/fashion-mnist --root $CHECKPOINT_DIR --save $EXPR_ID/vae --dataset fashion-mnist \
-      --batch_size 100 --epochs 200 --num_latent_scales 1 --num_groups_per_scale 2 --num_postprocess_cells 3 \
-      --num_preprocess_cells 3 --num_cell_per_cond_enc 1 --num_cell_per_cond_dec 1 --num_latent_per_group 20 \
-      --num_preprocess_blocks 2 --num_postprocess_blocks 2 --weight_decay_norm 1e-2 --num_channels_enc 64 \
-      --num_channels_dec 64 --decoder_dist bin --kl_anneal_portion 1.0 --kl_max_coeff 0.7 --channel_mult 1 2 2 \
-      --num_nf 0 --arch_instance res_mbconv --num_process_per_node 2 --use_se
+python3 train_vada.py --data $DATA_DIR/fashion-mnist --root $CHECKPOINT_DIR --save $EXPR_ID/lsgm --dataset fashion-mnist --epochs 800 \
+        --dropout 0.2 --batch_size 32 --num_scales_dae 2 --weight_decay_norm_vae 1e-2 \
+        --weight_decay_norm_dae 0. --num_channels_dae 256 --train_vae  --num_cell_per_scale_dae 8 \
+        --learning_rate_dae 3e-4 --learning_rate_min_dae 3e-4 --train_ode_solver_tol 1e-5 --cont_kl_anneal  \
+        --sde_type vpsde --iw_sample_p ll_iw --num_process_per_node 2 --use_se \
+        --vae_checkpoint $CHECKPOINT_DIR/EXPR_ID/vae/checkpoint.pt  --dae_arch ncsnpp --embedding_scale 1000 \
+        --mixing_logit_init -6 --warmup_epochs 20 --drop_inactive_var --skip_final_eval --fid_dir $FID_STATS_DIR
