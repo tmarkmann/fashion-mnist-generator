cdb51264aef5d6e22b40f899e799654fe12d6b30
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-22 23:01:52 +0000

    Adapted env name in slurm job.

--- slurm/inception_train.job.sh
+++ slurm/inception_train.job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /media/compute/homes/dmindlin/.bashrc
-conda activate fashion-mnist-generator
+conda activate fashion-mnist
 cd /media/compute/homes/dmindlin/fashion-mnist-generator
 
 python3 -m scripts.train_inception
