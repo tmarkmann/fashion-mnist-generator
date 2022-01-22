b78c78709bdbb02607f4f09a55edbede1079f985
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2022-01-22 15:51:25 +0100

    Add lsgm generate scripts

--- slurm/lsgm_generate.sh
+++ slurm/lsgm_generate.sh
@@ -10,7 +10,7 @@ export EXPR_ID=1
 cd ../LSGM
 python3 evaluate_vada.py --data $DATA_DIR/fashion-mnist --root $CHECKPOINT_DIR --save $EXPR_ID/eval --eval_mode sample \
         --checkpoint $CHECKPOINT_DIR/$EXPR_ID/lsgm/checkpoint.pt --num_process_per_node 2 --nll_ode_eval \
-        --ode_eps 1e-5 --ode_solver_tol 1e-5 --batch_size 128
+        --ode_eps 1e-5 --ode_solver_tol 1e-5 --batch_size 128 --num_fid_samples 1000
 
 cd ..
 python3 -m scripts.extract_npz --npz_dir $CHECKPOINT_DIR/$EXPR_ID/eval --output_dir $CHECKPOINT_DIR/$EXPR_ID/generated_images
