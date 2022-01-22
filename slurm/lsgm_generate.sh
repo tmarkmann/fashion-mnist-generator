f48422e6ff346a3c49059440062f222d614ada5c
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2022-01-22 15:53:34 +0100

    Reduc gpu count to 1 for lsgm generate

--- slurm/lsgm_generate.sh
+++ slurm/lsgm_generate.sh
@@ -9,7 +9,7 @@ export EXPR_ID=1
 
 cd ../LSGM
 python3 evaluate_vada.py --data $DATA_DIR/fashion-mnist --root $CHECKPOINT_DIR --save $EXPR_ID/eval --eval_mode sample \
-        --checkpoint $CHECKPOINT_DIR/$EXPR_ID/lsgm/checkpoint.pt --num_process_per_node 2 --nll_ode_eval \
+        --checkpoint $CHECKPOINT_DIR/$EXPR_ID/lsgm/checkpoint.pt --num_process_per_node 1 --nll_ode_eval \
         --ode_eps 1e-5 --ode_solver_tol 1e-5 --batch_size 128 --num_fid_samples 1000
 
 cd ..
