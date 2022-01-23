82073e25e690caf2c8f97c25c27d35fdd2d810de
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2022-01-23 18:52:32 +0100

    Update stylegan generate

--- slurm/stylegan_generate.sh
+++ slurm/stylegan_generate.sh
@@ -5,5 +5,5 @@ module load cuda/10.0
 export CUDA_HOME=/media/compute/vol/cuda/10.0
 
 cd ~/stylegan2-ada
-python generate.py --outdir=./results --trunc=1 --seeds=1-1000 \
+python generate.py --outdir=./results/generated_images --seeds=1-1000 \
     --network=./results/00009-fashionmnist-res32-auto1-noaug-resumecustom/network-snapshot-002416.pkl
