d9723836220b17977a9b20173d1be15e86405478
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-30 18:27:00 +0000

    Deleted inception score related slurm jobs.

--- slurm/inception_train.job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /media/compute/homes/dmindlin/.bashrc
-conda activate fashion-mnist
-cd /media/compute/homes/dmindlin/fashion-mnist-generator
-
-python3 -m scripts.train_inception
